580400669f64c5af6dcfb6240727691bea4233ea
hayder <hayderbd4290@gmail.com>
2024-10-04 02:03:29 +0600

    all project done

--- js/apps.js
+++ js/apps.js
@@ -12,9 +12,9 @@ const getCategory = async()=>{
    }
 };
 
-const getVideos = async()=>{
+const getVideos = async(shape="")=>{
     try{
-      const url = "https://openapi.programming-hero.com/api/phero-tube/videos" ;
+      const url = `https://openapi.programming-hero.com/api/phero-tube/videos${`?title=${shape}`}` ;
       const res = await fetch(url);
       const data = await res.json();
       displayVideo(data.videos);
@@ -56,9 +56,43 @@ const displayCategory = (category)=>{
    });  
 }
 
+const displayDetails = async(id)=>{
+  try{
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    document.getElementById("myModal").show();
+    
+    let ModalDiv = document.getElementById("modalDiv");
+    ModalDiv.innerHTML = `
+    <img class="mb-2" src=${data.video.thumbnail}>
+    <h1 class="text-xl font-semibold mb-2">${data.video.title}</h1>
+    <p>${data.video.description}</p>
+    `
+    
+  }
+  catch(err){
+   console.log(err)
+  }
+}
+
 const displayVideo = (videos)=>{
   const videoSection = document.getElementById("videoSection");
   videoSection.innerHTML = "";
+  if(videos.length===0){
+    videoSection.classList.remove("grid");
+    videoSection.innerHTML = `
+    <div class= "h-[600px] flex flex-col justify-center items-center">
+    <img class=" w-[160px] mb-12" src="./assets/icon.png">
+    <h1 class= "text-3xl font-bold">Oops!! Sorry, There is no content here</h1>
+    </div>
+    `
+    return;
+  }
+  else{
+    videoSection.classList.add("grid")
+  }
    videos.map(item =>{
     let div = document.createElement("div");
     div.classList.add("p-2");
@@ -85,20 +119,21 @@ const displayVideo = (videos)=>{
          ${item.authors[0].verified === true ? `<span><img class="w-5 h-5" src="./assets/icon2.png"></span>` : ""}
         
       </div>
-      <p class="ml-14">${item.others.views} Views</p>
+      <p class="ml-14 mb-4">${item.others.views} Views</p>
+      <button onclick= "displayDetails('${item.video_id}')" class="px-6 py-2 bg-btnBg text-white rounded-xl border-2 border-gray-300 text-xl ">Details</button>
     `
 
     videoSection.appendChild(div);
    })
   
   
-}
+};
 
-
-
-
-
+document.getElementById("input").addEventListener("keyup", (e)=>{
+  getVideos(e.target.value)
+}) 
 
 
 getCategory();
-getVideos();
+getVideos()
+
